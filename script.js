186c5478ed9e61e1bbbbe89fa33ad05362668c8c
josemgfortunato <jose.fortunato@live.com>
2022-07-19 23:53:33 -0300

    function

--- script.js
+++ script.js
@@ -1,89 +1,26 @@
-const payoutEl = document.querySelector("#payout");
-const peopleEl = document.querySelector("#people");
-const fiveEl = document.querySelector("#five");
-const tenEl = document.querySelector("#ten");
-const fifteenEl = document.querySelector("#fifteen");
-const twentyFiveEl = document.querySelector("#twenty-five");
-const fiftyEl = document.querySelector("#fifty");
+const payoutEl = document.querySelector("#payout")
+const peopleEl = document.querySelector("#people")
+const fiveEl = document.querySelector("#five")
+const tenEl = document.querySelector("#ten")
+const fifteenEl = document.querySelector("#fifteen")
+const twentyFiveEl = document.querySelector("#twenty-five")
+const fiftyEl = document.querySelector("#fifty")
 const customEl = document.querySelector("custom")
-const amountEl = document.querySelector("#amount");
-const totalEl = document.querySelector("#total");
-const resetEl = document.querySelector("#reset");
+const amountEl = document.querySelector("#amount")
+const totalEl = document.querySelector("#total")
+const resetEl = document.querySelector("#reset")
 
-fiveEl.addEventListener('click', fiveCalc);
-tenEl.addEventListener('click', tenCalc);
-fifteenEl.addEventListener('click', fifteenCalc);
-twentyFiveEl.addEventListener('click', twentyFiveCalc);
-fiftyEl.addEventListener('click', fiftyCalc);
-customEl.addEventListener('keypress', customCalc);
+fiveEl.addEventListener('click', splitter)
+// tenEl.addEventListener('click', splitter)
+// fifteenEl.addEventListener('click', splitter)
+// twentyFiveEl.addEventListener('click', splitter)
+// fiftyEl.addEventListener('click', splitter)
 
-function fiveCalc(p) {
-    const percentage = parseInt(payoutEl.value) / 20;
-    const tip = percentage + parseInt(payoutEl.value);
-    const people = parseInt(peopleEl.value);
-    const total = parseInt(tip / people);
-    amountEl.innerHTML = `$ ${tip}`;
-    totalEl.innerHTML = `$ ${total}`;
-    p.preventDefault();
-}
-
-function tenCalc(p) {
-    const percentage = parseInt(payoutEl.value) / 10;
-    const tip = percentage + parseInt(payoutEl.value);
-    const people = parseInt(peopleEl.value);
-    const total = parseInt(tip / people);
-    amountEl.innerHTML = `$ ${tip}`;
-    totalEl.innerHTML = `$ ${total}`;
-    p.preventDefault();
-}
-
-function fifteenCalc(p) {
-    const percentage = parseInt(payoutEl.value) / 6.66666667;
-    const tip = percentage + parseInt(payoutEl.value);
-    const people = parseInt(peopleEl.value);
-    const total = parseInt(tip / people);
-    amountEl.innerHTML = `$ ${tip}`;
-    totalEl.innerHTML = `$ ${total}`;
-    p.preventDefault();
-}
-
-function twentyFiveCalc(p) {
-    const percentage = parseInt(payoutEl.value) / 4;
-    const tip = percentage + parseInt(payoutEl.value);
-    const people = parseInt(peopleEl.value);
-    const total = parseInt(tip / people);
-    amountEl.innerHTML = `$ ${tip}`;
-    totalEl.innerHTML = `$ ${total}`;
-    p.preventDefault();
-}
-
-function fiftyCalc(p) {
-    const percentage = parseInt(payoutEl.value) / 2;
-    const tip = percentage + parseInt(payoutEl.value);
-    const people = parseInt(peopleEl.value);
-    const total = parseInt(tip / people);
-    amountEl.innerHTML = `$ ${tip}`;
-    totalEl.innerHTML = `$ ${total}`;
-    p.preventDefault();
-}
-
-function customCalc(p) {
-    const payout = parseInt(payoutEl.value);
-    const people = parseInt(peopleEl.value);
-    const custom = parseInt(customEl.value);
-    const customPercentage = (100 / custom);
-    const percentage =  payout / customPercentage;
-    const tip = percentage + payout;
-    const total = tip / people;
-    amountEl.innerHTML = `$ ${tip}`;
-    totalEl.innerHTML = `$ ${total}`;
-
-    const tecla = p.key;
-    if (key === `Enter`) {
-        document.querySelector("custom").keypress();
-    }
-
-    p.preventDefault();
-
-    
+function splitter(p) {
+    const percentage = (Number(payoutEl.value) / 20)
+    const tip = (Number(payoutEl.value) + percentage).toFixed(2)
+    const total = (tip / Number(peopleEl.value).toFixed(2))
+    amountEl.innerText = `$ ${tip}`
+    totalEl.innerText = `$ ${total}`
+    p.preventDefault()
 }
